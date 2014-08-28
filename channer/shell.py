9e469e29d72bdc626ee23e203f89c1995286bbdd
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2014-08-28 16:35:34 +0900

    Fixed comments up

--- channer/shell.py
+++ channer/shell.py
@@ -1,5 +1,5 @@
 # -*- mode: python, coding: utf-8 -*-
-# shell.py: Shell entrypoint
+# shell.py: Shell entrypoints.
 # (C) 2014 Takahiro Yoshimura <altakey@gmail.com>.  All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
